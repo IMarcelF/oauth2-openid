6709c4c8340f5febad77a28192d027e3e00a4bbb
Marcel Iekiny <iekinyfernandes@gmail.com>
2017-10-05 17:51:23 -0100

    ...

--- src/main/java/igrp/helper/OAuth2Helper.java
+++ src/main/java/igrp/helper/OAuth2Helper.java
@@ -7,6 +7,31 @@ public final class OAuth2Helper { // Not inherit ...
 	
 	private OAuth2Helper() {} // Not instantiate ...
 	
-	/* A set of public static methods */
+	/*
+	 * * A set of public static methods ... **/
 	
+	// OAuth2 response_type=code
+	public static void authorizationCodeGrant(String client_id, String scope, String redirect_uri) {
+		
+	}
+	
+	// OAuth2 grant_type=authorization_code
+	public static void swapCodeByToken(String code, String client_id, String client_secret, String redirect_uri) {
+		
+	}
+	
+	// OAuth2 response_type=token & grant_type=implicit
+	public static void implicitGrant(String client_id, String scope, String redirect_uri) {
+		
+	}
+	
+	// OAuth2 grant_type=password
+	public static void resourceOwnerPasswordGrant(String username, String password, String client_id, String client_secret, String scope) {
+		
+	}
+	
+	// OAuth2 grant_type=client_credentials
+	public static void clientCredentilasGrant(String client_id, String client_secret) {
+		
+	}
 }

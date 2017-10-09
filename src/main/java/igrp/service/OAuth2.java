717d7d8aae1ac679e136a865ebb6bd0ff2cbfb57
Marcel Iekiny <iekinyfernandes@gmail.com>
2017-10-09 12:16:56 -0100

    Define oauth2 resources

--- src/main/java/igrp/service/OAuth2.java
+++ src/main/java/igrp/service/OAuth2.java
@@ -2,19 +2,14 @@ package igrp.service;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.core.MediaType;
-
 import org.apache.log4j.Logger;
-
-import igrp.resource.Employee;
 
 @Path("/oauth2")
 public class OAuth2 {

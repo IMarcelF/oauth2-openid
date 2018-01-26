40104f93cf503bc9a626037608bdcdfdfd3608aa
Iekiny Marcel <Iekiny Marcel@10.73.96.34>
2018-01-26 19:25:14 -0100

    Implement the listener

---
+++ src/main/java/igrp/listener/DAOHelperListener.java
@@ -0,0 +1,23 @@
+package igrp.listener;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+import igrp.helper.DAOHelper;
+
+/**
+ * Iekiny Marcel
+ * Jan 26, 2018
+ */
+@WebListener
+public class DAOHelperListener implements ServletRequestListener{
+	
+	 public void requestDestroyed(ServletRequestEvent arg0)  { 
+		 System.out.println("Entrado ...");
+    	 DAOHelper.getInstance().closeCurrentSession();
+    }
+
+    public void requestInitialized(ServletRequestEvent arg0)  {}    
+
+}
